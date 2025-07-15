146a8d08665d619ab3e66047638d2b2caba90303
tlecobra7 <Abdul.muhamad96@gmail.com>
2025-07-15 16:05:07 +0700

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from bot import web_app
-
-if __name__ == "__main__":
-    web_app.run()
-```    *   **Apa artinya ini?** File ini hanya bertugas satu hal: mengimpor objek `web_app` (aplikasi Flask Anda) dari file `bottle.py`.
